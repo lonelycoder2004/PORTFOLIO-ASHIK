6ec1e19808e32b13f61659e5f7048f51bea05b99
Ashik Shaji <ashikshaji4u@gmail.com>
2025-07-26 11:47:22 +0530

    resume updated

--- FRONTEND/my-portfolio/src/components/AboutSection.jsx
+++ FRONTEND/my-portfolio/src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ function AboutSection() {
                 Get In Touch
               </a>
               <a
-                href="/Ashik_Shaji_Resume.pdf"
+                href="/AshikShaji_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 download
               >
